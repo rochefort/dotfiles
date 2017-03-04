563f234ff5e6f2833b84354a3a02e24f2fa2f189
rochefort <terasawan@gmail.com>
2017-03-04 19:48:09 +0900

    Use watcher on hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,10 @@
 ---------------------------------------------------------
 -- General
 ---------------------------------------------------------
-local function remap(key, mods)
+local function keyStroke(key, mods)
   mods = mods or {}
   return function()
-    hs.eventtap.keyStroke(mods, key)
+    hs.eventtap.keyStroke(mods, key, 1000)
   end
 end
 
@@ -16,17 +16,51 @@ local function enableAll(keySet)
   for k, v in pairs(keySet) do v:enable() end
 end
 
+local function has_value (tab, val)
+  for index, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+  return false
+end
+
+---------------------------------------------------------
+-- Key Binding
+---------------------------------------------------------
+local vimBinding = {
+  hs.hotkey.new({'ctrl'}, 'J', keyStroke('down')),
+  hs.hotkey.new({'ctrl'}, 'K', keyStroke('up'))
+}
+
+local whiteListApps = {
+  "Dash", "Evernote"
+}
+
+local function handleGlobalAppEvent(name, event, app)
+  if event == hs.application.watcher.activated then
+    if has_value(whiteListApps, name) then
+      enableAll(vimBinding)
+    else
+      disableAll(vimBinding)
+    end
+  end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
 ---------------------------------------------------------
 -- DashのみでVim Bindingを適用
 ---------------------------------------------------------
-local vimBinding = {
-  hs.hotkey.new({'ctrl'}, 'J', remap('down')),
-  hs.hotkey.new({'ctrl'}, 'K', remap('up'))
-}
+-- local vimBinding = {
+--   hs.hotkey.new({'ctrl'}, 'J', keyStroke('down')),
+--   hs.hotkey.new({'ctrl'}, 'K', keyStroke('up'))
+-- }
 
-hs.window.filter.new('Dash')
-  :subscribe(hs.window.filter.windowFocused, function() enableAll(vimBinding) end)
-  :subscribe(hs.window.filter.windowUnfocused, function() disableAll(vimBinding) end)
+-- hs.window.filter.new('Dash')
+--   :subscribe(hs.window.filter.windowFocused, function() enableAll(vimBinding) end)
+--   :subscribe(hs.window.filter.windowUnfocused, function() disableAll(vimBinding) end)
 
 ---------------------------------------------------------
 -- Evernote
