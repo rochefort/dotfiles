a59d9c718ad8c09016987495927b1336675333ca
rochefort <terasawan@gmail.com>
2017-03-06 23:40:58 +0900

    Adopted black list sytem for key remapping
    Becouse I cannot manipulate Spotlight and Alfred windows

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -29,25 +29,29 @@ end
 -- Key Binding
 ---------------------------------------------------------
 local vimBinding = {
-  hs.hotkey.new({'ctrl'}, 'J', keyStroke('down')),
-  hs.hotkey.new({'ctrl'}, 'K', keyStroke('up'))
+  hs.hotkey.bind({'ctrl'}, 'J', keyStroke('down')),
+  hs.hotkey.bind({'ctrl'}, 'K', keyStroke('up'))
 }
 
-local whiteListApps = {
-  "Dash", "Evernote"
+-- local vimBindingApps = {
+--   "Alfred 3", "Dash", "Evernote"
+-- }
+local notVimBindingApps = {
+  "Atom", "CotEditor", "iTerm", "Google Chrome"
 }
 
 local function handleGlobalAppEvent(name, event, app)
   if event == hs.application.watcher.activated then
-    if hasValue(whiteListApps, name) then
-      enableAll(vimBinding)
-    else
+    -- hs.alert(name)
+    if hasValue(notVimBindingApps, name) then
       disableAll(vimBinding)
+    else
+      enableAll(vimBinding)
     end
   end
 end
 
-appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+local appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
 appsWatcher:start()
 
 ---------------------------------------------------------
@@ -61,6 +65,10 @@ appsWatcher:start()
 -- hs.window.filter.new('Dash')
 --   :subscribe(hs.window.filter.windowFocused, function() enableAll(vimBinding) end)
 --   :subscribe(hs.window.filter.windowUnfocused, function() disableAll(vimBinding) end)
+
+-- hs.window.filter.default:allowApp('SystemUIServer')
+-- hs.window.filter.new('Spotlight')
+--   :subscribe(hs.window.filter.windowFocused, function() alert("aaa") end)
 
 ---------------------------------------------------------
 -- Evernote
@@ -77,15 +85,15 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
     end
-    if doReload then
-        hs.reload()
-    end
+  end
+  if doReload then
+    hs.reload()
+  end
 end
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
